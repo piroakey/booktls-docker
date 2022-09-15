f559549b7b55b28b9f4672ca68d6995097ea60a9
piroakey <piroakey@gmail.com>
2022-09-16 00:34:55 +0900

    PID1問題に対応

--- booktls-client/init.sh
+++ booktls-client/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+trap "exit 0" SIGTERM SIGINT
+
 export PATH="/opt/openssl/bin:$PATH"
 export LD_LIBRARY_PATH="/opt/openssl/lib"
 export PKG_CONFIG_PATH="/opt/openssl/lib/pkgconfig"
