eeadb92b7d7b32fc16d8ad3f79a168a4c550f414
gwrhyr <acrographia+dev@gmail.com>
2024-05-06 02:11:55 +0900

    Windowsで発生するバグを修正 - Dockerfileに改行コードを変換する処理を追加 	- init.shの改行コードがWindows式(CRLF)のため常にLinux式(LF)になるように変更 	- WSL2 バックエンドDocker Desktopでdocker buildした際にエラーとなったため - init.sh内のパスを64bit端末に対応 	- 64bit端末でdocker runした際に、libではなくlib64にてディレクトリが作成されたため。

--- openssl/init.sh
+++ openssl/init.sh
@@ -3,8 +3,14 @@
 trap "exit 0" SIGTERM SIGINT
 
 export PATH="/opt/openssl/bin:$PATH"
-export LD_LIBRARY_PATH="/opt/openssl/lib"
-export PKG_CONFIG_PATH="/opt/openssl/lib/pkgconfig"
+
+if [ -e "/opt/openssl/lib" ]; then
+	export LD_LIBRARY_PATH="/opt/openssl/lib"
+	export PKG_CONFIG_PATH="/opt/openssl/lib/pkgconfig"
+elif [ -e "/opt/openssl/lib64" ]; then # Path for 64 bit host 
+	export LD_LIBRARY_PATH="/opt/openssl/lib64"
+	export PKG_CONFIG_PATH="/opt/openssl/lib64/pkgconfig"
+fi
 
 openssl version
 
