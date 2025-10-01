f699bd230f46b16b8fba3e8b926186c10362d7d3
Takio Yamaoka <y.takio@gmail.com>
2025-10-01 13:34:22 +0900

    fix path selection

--- openssl/init.sh
+++ openssl/init.sh
@@ -4,12 +4,13 @@ trap "exit 0" SIGTERM SIGINT
 
 export PATH="/opt/openssl/bin:$PATH"
 
+if [ -d /opt/openssl/lib ]; then
+  export LD_LIBRARY_PATH="/opt/openssl/lib:$LD_LIBRARY_PATH"
+  export PKG_CONFIG_PATH="/opt/openssl/lib/pkgconfig:$PKG_CONFIG_PATH"
+fi
 if [ -d /opt/openssl/lib64 ]; then
   export LD_LIBRARY_PATH="/opt/openssl/lib64:$LD_LIBRARY_PATH"
   export PKG_CONFIG_PATH="/opt/openssl/lib64/pkgconfig:$PKG_CONFIG_PATH"
-elif [ -d /opt/openssl/lib ]; then
-  export LD_LIBRARY_PATH="/opt/openssl/lib:$LD_LIBRARY_PATH"
-  export PKG_CONFIG_PATH="/opt/openssl/lib/pkgconfig:$PKG_CONFIG_PATH"
 fi
 
 openssl version
